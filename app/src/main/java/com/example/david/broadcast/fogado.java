8ee67445075fd8bbe43d13e0304c9b641396ffbf
erdeia90 <erdeilink@gmail.com>
2018-12-08 18:49:41 +0100

    konstruktor/ERtesites letrehozasa

--- app/src/main/java/com/example/david/broadcast/fogado.java
+++ app/src/main/java/com/example/david/broadcast/fogado.java
@@ -3,13 +3,19 @@ package com.example.david.broadcast;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class fogado extends BroadcastReceiver {
 
+    public static final String Ertesites = "com.example.david.broadcast.Ertesites";
+
+    public fogado(){
+        // üres konstruktor
+    }
+
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
+
         throw new UnsupportedOperationException("Not yet implemented");
     }
 }
