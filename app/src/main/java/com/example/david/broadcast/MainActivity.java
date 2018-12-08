e8439f8f0e99217a1a42d6d749aef0d020d6e7e9
David Bukovszki <bukovszki.david@gmail.com>
2018-12-08 18:26:39 +0100

    start

---
+++ app/src/main/java/com/example/david/broadcast/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.david.broadcast;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
