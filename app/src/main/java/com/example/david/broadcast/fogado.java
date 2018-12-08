d0a381365c52c4884976ac6bb84fbd23f8001eeb
erdeia90 <erdeilink@gmail.com>
2018-12-08 19:26:33 +0100

    kenyér (toast) elkészítése

--- app/src/main/java/com/example/david/broadcast/fogado.java
+++ app/src/main/java/com/example/david/broadcast/fogado.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
 
+
+
 public class fogado extends BroadcastReceiver {
 
     public static final String Ertesites = "com.example.david.broadcast.Ertesites";
@@ -16,6 +18,14 @@ public class fogado extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
-        throw new UnsupportedOperationException("Not yet implemented");
+        String segeduzenet = intent.getStringExtra("felado");
+        mutasd("Ertesitésed érkezett: " +segeduzenet, context);
+
+    }
+
+    private void mutasd (String felado, Context context){
+        Toast.makeText(context,felado, Toast.LENGTH_LONG).show();
+      //  Toast.makeText(context,targy,Toast.LENGTH_SHORT).show();
+
     }
 }
