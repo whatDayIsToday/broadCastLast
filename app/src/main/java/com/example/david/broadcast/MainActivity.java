804ea465031db5617c15d171b99903c626e923de
David Bukovszki <bukovszki.david@gmail.com>
2018-12-08 19:01:05 +0100

    Textboxokhoz tartozo adatok kiolvasasa segedvaltozokba, valamint uj Intent létrehozása, és helyi BC beállítása stb.

--- app/src/main/java/com/example/david/broadcast/MainActivity.java
+++ app/src/main/java/com/example/david/broadcast/MainActivity.java
@@ -12,9 +12,49 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ComponentName componentName;
+    private PackageManager packageManager;
+
+    //fogadó osztaly peldanyositasa
+    private fogado receiver = new fogado();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Gomb kattintásfigyelő vezérlő meghívása, majd értesítés küldése.
+
+        findViewById(R.id.bcGomb).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
+    public void uzenetMutato(){
+
+        // a bemeneti adatok elmentése két segédváltozóba
+
+        String felado = ((EditText) findViewById(R.id.mailCimBox)).getText().toString();
+        String targy = ((EditText) findViewById(R.id.targybox)).getText().toString();
+
+        // a fogadó osztályból érkező "Ertesites" lekezelése
+
+        Intent seged = new Intent(fogado.Ertesites);
+
+        // segédváltozók átadása
+
+        seged.putExtra("felado",felado);
+        seged.putExtra("targy",targy);
+
+        //Helyi broadcast küldése
+
+        LocalBroadcastManager.getInstance(this).sendBroadcast(seged);
+
+
+
+    }
+
 }
